3fad7812bfed025d68db8ff39653afa8ab636c36
geestg <glenstg15@gmail.com>
2025-11-05 21:57:20 +0700

    progress dataset weight

--- rename.py
+++ rename.py
@@ -1,7 +1,7 @@
 import os
 
 # Path folder gambar
-folder_path = r"D:\SEMESTER 5\TA 1\eFishery-iot-cv\efishery_yolov8\dataset\images\train"
+folder_path = r"D:\eFishery-cv\efishery_yolov8\weight\frame"
 
 # Ambil semua file gambar
 files = os.listdir(folder_path)
